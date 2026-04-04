c268a6a7a5ffdbfe3ac4ae69157ae5f5bd30985f
Vincent <wuzhang1844@gmail.com>
2026-04-04 16:32:30 -0700

    Add: gradient-descent - submission-0

---
+++ Machine Learning/gradient-descent/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
+        # Objective function: f(x) = x^2
+        # Derivative:         f'(x) = 2x
+        # Update rule:        x = x - learning_rate * f'(x)
+        # Round final answer to 5 decimal places
+        if iterations == 0:
+            return init
+        
+        xo = init
+        xn = xo
+
+        for i in range(iterations):
+            xn = xn - learning_rate * 2*xn
+        
+        return round(xn, 5)
+
